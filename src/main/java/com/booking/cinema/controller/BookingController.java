43168f1b4599cbaf2b7d0afb6488b15746e5d967
Thales Oliveira S. Costa <thalesslva67@gmail.com>
2026-05-26 13:59:11 -0300

    feat(controller): create booking controller

---
+++ src/main/java/com/booking/cinema/controller/BookingController.java
@@ -0,0 +1,50 @@
+package com.booking.cinema.controller;
+
+import com.booking.cinema.dto.request.BookingRequestDTO;
+import com.booking.cinema.dto.response.BookingResponseDTO;
+import com.booking.cinema.service.BookingService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/bookings")
+public class BookingController {
+    private final BookingService bookingService;
+    
+    public BookingController(BookingService bookingService) {
+        this.bookingService = bookingService;
+    }
+
+    @PostMapping()
+    public ResponseEntity<BookingResponseDTO> create(@Valid @RequestBody BookingRequestDTO request) {
+        BookingResponseDTO response = bookingService.save(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BookingResponseDTO> findById(@PathVariable Long id) {
+        BookingResponseDTO response = bookingService.findById(id);
+
+        return ResponseEntity.ok(response);
+    }
+    
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        bookingService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<BookingResponseDTO>> getAll() {
+        List<BookingResponseDTO> bookings = bookingService.findAll();
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(bookings);
+    }
+}
