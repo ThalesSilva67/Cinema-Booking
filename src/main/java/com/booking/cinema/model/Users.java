0f1a1e55edf02ecbcb8e9c72e0c565ba929595af
Thales Oliveira S. Costa <thalesslva67@gmail.com>
2026-05-26 13:07:03 -0300

    refactor(user): improve readability and eliminate code duplication

--- src/main/java/com/booking/cinema/model/Users.java
+++ src/main/java/com/booking/cinema/model/Users.java
@@ -40,6 +40,12 @@ public class Users {
         this.role = role;
     }
 
+    public Users(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
     public Long getId() {
         return id;
     }
--- src/main/java/com/booking/cinema/service/UserService.java
+++ src/main/java/com/booking/cinema/service/UserService.java
@@ -1,28 +1,34 @@
 package com.booking.cinema.service;
 
+import com.booking.cinema.dto.request.LoginRequestDTO;
 import com.booking.cinema.dto.request.RegisterRequestDTO;
 import com.booking.cinema.dto.response.LoginResponseDTO;
 import com.booking.cinema.dto.response.UserResponseDTO;
+import com.booking.cinema.exception.DuplicateEmailException;
+import com.booking.cinema.exception.InvalidCredentialsException;
+import com.booking.cinema.mapper.UserMapper;
 import com.booking.cinema.model.Users;
 import com.booking.cinema.model.Role;
 import com.booking.cinema.repository.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository,  UserMapper userMapper) {
         this.userRepository = userRepository;
+        this.userMapper = userMapper;
     }
 
-    public UserResponseDTO registerStandardUser(RegisterRequestDTO request) {
+    public UserResponseDTO register(RegisterRequestDTO request) {
         userRepository.findByEmail(request.email()).ifPresent(user -> {
-            throw new IllegalStateException("Email  already exists");
+            throw new DuplicateEmailException("Email already exists");
         });
 
         Users user = new Users();
@@ -33,76 +39,48 @@ public class UserService {
 
         Users saved = userRepository.save(user);
 
-        return new UserResponseDTO(saved.getId(), saved.getName(), saved.getEmail());
+        return userMapper.toUserResponseDTO(saved);
     }
 
-    public UserResponseDTO registerAdmUser(RegisterRequestDTO request) {
-        userRepository.findByEmail(request.email()).ifPresent(user -> {
-            throw new IllegalArgumentException("Email already exists");
-        });
-
-        Users user = new Users();
-        user.setName(request.name());
-        user.setEmail(request.email());
-        user.setPassword(request.password());
-        user.setRole(Role.ADMINISTRADOR);
-
-        Users saved = userRepository.save(user);
-
-        return new UserResponseDTO(saved.getId(), saved.getName(), saved.getEmail());
-    }
-
-    public LoginResponseDTO loginUser(String email, String password) {
-        Users user = userRepository.findByEmail(email).orElseThrow(() -> new IllegalArgumentException("Invalid credentials"));
-        if(!password.equals(user.getPassword())) {
-            throw new  IllegalArgumentException("Invalid credentials");
+    public LoginResponseDTO login(LoginRequestDTO request) {
+        Users user = userRepository.findByEmail(request.email()).orElseThrow(() -> new InvalidCredentialsException("Invalid credentials"));
+        if (!request.password().equals(user.getPassword())) {
+            throw new InvalidCredentialsException("Invalid credentials");
         }
-
-        return new LoginResponseDTO("Login realizado com sucesso!");
+        return userMapper.toLoginResponseDTO("Login realizado com sucesso!");
     }
 
-    public List<Users> findAll() {
-        List<Users> list = userRepository.findAll();
-
-        if (list.isEmpty()) return new ArrayList<>();
-
-        return list;
+    public List<UserResponseDTO> findAll() {
+        return userRepository.findAll().stream().map(userMapper::toUserResponseDTO).toList();
     }
 
-    public Users findById(Long id) {
-        return userRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Users not found with ID: " + id));
+    public UserResponseDTO findById(Long id) {
+        Users user = userRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("User not found"));
+        return userMapper.toUserResponseDTO(user);
     }
 
     public void delete(Long id) {
-        if(!userRepository.existsById(id)){
+        if (!userRepository.existsById(id)) {
             throw new EntityNotFoundException("User not found");
         }
-
         userRepository.deleteById(id);
     }
 
-    public UserResponseDTO updateStandard(Long id, RegisterRequestDTO user) {
-        Users oldUsers =  userRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Users not found"));
+    public UserResponseDTO update(Long id, RegisterRequestDTO user) {
+        Users oldUser = userRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Users not found"));
 
-        oldUsers.setName(user.name());
-        oldUsers.setEmail(user.email());
-        oldUsers.setPassword(user.password());
+        Optional<Users> existingUser = userRepository.findByEmail(user.email());
+        if (existingUser.isPresent() && !existingUser.get().getId().equals(id)) {
+            throw new DuplicateEmailException("Email already exists");
+        }
 
-        Users update = userRepository.save(oldUsers);
+        oldUser.setName(user.name());
+        oldUser.setEmail(user.email());
+        oldUser.setPassword(user.password());
 
-        return new UserResponseDTO(update.getId(), update.getName(), update.getEmail());
-    }
+        Users updated = userRepository.save(oldUser);
 
-    public UserResponseDTO updateAdm(Long id, RegisterRequestDTO user) {
-        Users oldUsers =  userRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Users not found"));
-
-        oldUsers.setName(user.name());
-        oldUsers.setEmail(user.email());
-        oldUsers.setPassword(user.password());
-
-        Users update = userRepository.save(oldUsers);
-
-        return new UserResponseDTO(update.getId(), update.getName(), update.getEmail());
+        return userMapper.toUserResponseDTO(updated);
     }
 
 }
