85cadb8bb3c64382658444b8428308cd162d52aa
Thales Oliveira S. Costa <thalesslva67@gmail.com>
2026-05-26 13:05:48 -0300

    feat(mapper): create userMapper to add in service user

---
+++ src/main/java/com/booking/cinema/mapper/UserMapper.java
@@ -0,0 +1,21 @@
+package com.booking.cinema.mapper;
+
+import com.booking.cinema.dto.response.LoginResponseDTO;
+import com.booking.cinema.dto.response.UserResponseDTO;
+import com.booking.cinema.model.Users;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+    public UserResponseDTO toUserResponseDTO(Users user) {
+        return new UserResponseDTO(user.getId(), user.getName(), user.getEmail());
+    }
+
+    public Users toUsers(UserResponseDTO userResponseDTO) {
+        return new Users(userResponseDTO.id(), userResponseDTO.name(), userResponseDTO.email());
+    }
+
+    public LoginResponseDTO toLoginResponseDTO(String message) {
+        return new LoginResponseDTO(message);
+    }
+}
