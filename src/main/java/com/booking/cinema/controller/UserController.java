1a854aecfe8794ad4f055efa056609181a03b133
Thales Oliveira S. Costa <thalesslva67@gmail.com>
2026-05-26 18:38:46 -0300

    feat(userController): create userController to create admins users

---
+++ src/main/java/com/booking/cinema/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.booking.cinema.controller;
+
+import com.booking.cinema.dto.request.RegisterRequestDTO;
+import com.booking.cinema.dto.response.UserResponseDTO;
+import com.booking.cinema.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/admin")
+    public ResponseEntity<UserResponseDTO> register(@Valid @RequestBody RegisterRequestDTO request) {
+        UserResponseDTO response = userService.registerAdmin(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+}
